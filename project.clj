34fbbcc6ed14ffaa03fcf823e7df11e5774f92fc
apeckham <apeckham@gmail.com>
2017-06-03 14:27:01 -0700

    hello world

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cc.qbits/spandex "0.3.11"]
+                 [faker "0.2.2"]]
   :main ^:skip-aot es-typeahead.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/es_typeahead/core.clj
+++ src/es_typeahead/core.clj
@@ -1,7 +1,24 @@
 (ns es-typeahead.core
+  (:require [qbits.spandex :as s])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, asdf World!"))
+
+(->> {:url "/blog/user/dilbert"
+      :method :put
+      :body {:name "Dilbert Brown"}
+      :headers {:content-type "application/json"}}
+     (s/request (s/client)))
+
+(->> {:url "/blog/user/_search"
+      :method :get
+      :body {:query {:match_all {}}}
+      :headers {:content-type "application/json"}}
+     (s/request (s/client))
+     :body
+     :hits
+     :hits
+     pprint)
