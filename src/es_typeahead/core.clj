50024938a9b8411328b69037bdfa578352c6a62d
apeckham <apeckham@gmail.com>
2017-06-03 21:52:57 -0700

    bulk indexing!

--- src/es_typeahead/core.clj
+++ src/es_typeahead/core.clj
@@ -1,7 +1,8 @@
 (ns es-typeahead.core
-  (:require [qbits.spandex :as s]
-            [faker.name :refer [names]])
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.core.async :as async]
+            [faker.name :refer [names]]
+            [qbits.spandex :as s]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -32,51 +33,64 @@
        (map :_source)
        pprint))
 
-
 (comment "easy indexing"
          (doall (map index (take 10 (names))))
 
          (count)
 
-         (match-all)
-
-         )
+         (match-all))
 
 (comment "completion suggester"
 
-  (req "/music" :delete nil)
+         (req "/music" :delete nil)
 
-  (req "/music" :put {:mappings
-                      {:song
-                       {:properties
-                        {:suggest {:type "completion"}
-                         :title {:type "keyword"}}}}})
+         (req "/music" :put {:mappings
+                             {:song
+                              {:properties
+                               {:suggest {:type "completion"}
+                                :title {:type "keyword"}}}}})
 
-  (doseq [song (take 1000 (names))]
-    (req "/music/song" :post {:additional "payload"
-                              :suggest
-                              {:input song}}))
+         (doseq [song (take 1000 (names))]
+           (req "/music/song" :post {:additional "payload"
+                                     :suggest
+                                     {:input song}}))
 
-  (:count (req "/music/song/_count" :post nil))
+         (let [{:keys [input-ch output-ch]} (s/bulk-chan client)
+               action (fn [name]
+                        [{:index {:_index "music"
+                                  :_type "song"}}
+                         {:additional "payload2"
+                          :suggest {:input name}}])]
 
-  (->> {:query {:match_all {}}}
-       (req "/music/_search" :post)
-       :hits
-       :hits
-       (map :_source))
+           (async/put! input-ch (mapcat action (take 10000 (names))))
+           (async/close! input-ch)
+           (loop []
+             (when-let [[job response] (async/<!! output-ch)]
+               (if (-> response :body :errors)
+                 (throw (Exception. "Bulk index failed")))
+               (recur))))
 
-  (defn suggest [prefix]
-    (->> {:suggest
-          {:song-suggest
-           {:prefix prefix
-            :completion {:field "suggest"}}}}
-         (req "/music/_search" :post)
-         :suggest
-         :song-suggest
-         first
-         :options
-         (map :_source)))
+         (:count (req "/music/song/_count" :post nil))
 
-  (suggest "A")
+         (->> {:query {:match_all {}}}
+              (req "/music/_search" :post)
+              :hits
+              :hits
+              (map :_source))
 
-  )
+         (defn suggest [prefix]
+           (->> {:suggest
+                 {:song-suggest
+                  {:prefix prefix
+                   :completion {:field "suggest"}}}}
+                (req "/music/_search" :post)
+                :suggest
+                :song-suggest
+                first
+                :options
+                (map :_source)))
+
+         (suggest "A")
+
+
+         )
