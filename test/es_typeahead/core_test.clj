3117393ff9a8fe9fc00601439732865787b92c77
apeckham <apeckham@gmail.com>
2017-06-04 17:16:00 -0700

    refactoring

--- test/es_typeahead/core_test.clj
+++ test/es_typeahead/core_test.clj
@@ -16,14 +16,18 @@
                                   :response {:status 200
                                              :body "{\"message\": \"Hello World\"}"
                                              :headers {:Content-Type "application/json"}}})
-    (let [response @(http/get (format "http://localhost:%d/hello" (.port server)))]
-      (is (= {"message" "Hello World"} (json/parse-string (:body response))))))
+    (is (= {"message" "Hello World"} (-> (format "http://localhost:%d/hello" (.port server))
+                                         http/get
+                                         deref
+                                         :body
+                                         json/parse-string))))
 
   (testing "spandex request"
     (wiremock/new-mapping server {:request {:method "POST" :url "/blog/user"}
                                   :response {:status 200
                                              :body "{}"
                                              :headers {:Content-Type "application/json"}}})
+
     (let [client (s/client {:hosts [(str "http://localhost:" (.port server))]})]
       (s/request client {:url "/blog/user"
                          :method :post
@@ -33,6 +37,7 @@
                          :method :post
                          :body {:name "hello"}
                          :headers {:content-type "application/json"}}))
+
     (is (= ["/blog/user" "/blog/user"] (map :url (:requests (wiremock/find-requests server {:method "POST"})))))
     (is (= 2 (wiremock/count-requests server {:method "POST"})))
     (is (empty? (:requests (wiremock/unmatched-requests server))))))
